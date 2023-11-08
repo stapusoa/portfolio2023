6fe0a5d8a5b707fe4b9f03988f26c36141c4a0dd
Sara Tapusoa <sara.tapusoa@gmail.com>
2023-11-08 13:04:54 -0700

    round 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 // pages/index.js
 import Head from 'next/head';
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
@@ -14,18 +16,18 @@ export default function Home() {
         <div className="hero-section">
           <div className="hero-container">
             <div className="hero-header">
-              <h4 className="text-4xl font-bold text-gray-900 dark:text-white">
+              <h4 className="text-left">
                 product designer
               </h4>
-              <h1 className="mt-4 text-xl">
+              <h1 className="text-left">
                 designing for users, <br />not just pixels.
               </h1>
-              <div className="mt-8 flex gap-4">
+              <div className="button-double-container">
                 <button className="button-primary">
-                  Button 1
+                  projects
                 </button>
                 <button className="button-secondary">
-                  Button 2
+                  github
                 </button>
               </div>
             </div>
@@ -33,12 +35,58 @@ export default function Home() {
         </div>
         <div className="content-container">
           <div className="section-txt-img-center">
+            <h5 className="text-center">
+                fishbowl go
+            </h5>
+            <h3 className="text-center">
+              simplifying inventory management in the warehouse and in the office.
+            </h3>
+            <div className="proj1-img-container">
+              <Image
+                  src="/project-image1.png" // The path to your image file, relative to the public directory
+                  alt="Project Image" // Alternative text for the image
+                  width={900} // The width of the image in pixels
+                  height={300} // The height of the image in pixels
+                  layout="intrinsic" // Optional: Defines how the image should be resized and positioned
+                />   
+            </div>
+             
 
           </div>
           <div className="section-txt-img-left">
+            <h5 className="text-left">
+                fishbowl go
+            </h5>
+            <h3 className="text-left">
+              simplifying inventory management in the warehouse and in the office.
+            </h3>
+            <div className="proj1-img-container">
+              <Image
+                  src="/project-image1.png" // The path to your image file, relative to the public directory
+                  alt="Project Image" // Alternative text for the image
+                  width={900} // The width of the image in pixels
+                  height={300} // The height of the image in pixels
+                  layout="intrinsic" // Optional: Defines how the image should be resized and positioned
+                />   
+            </div>
             
           </div>
           <div className="section-txt-img-right">
+            <h5 className="text-right">
+                fishbowl go
+            </h5>
+            <h3 className="text-right">
+              simplifying inventory management in the warehouse and in the office.
+            </h3>
+            <div className="proj1-img-container">
+              <Image
+                  src="/project-image1.png" // The path to your image file, relative to the public directory
+                  alt="Project Image" // Alternative text for the image
+                  width={900} // The width of the image in pixels
+                  height={300} // The height of the image in pixels
+                  layout="intrinsic" // Optional: Defines how the image should be resized and positioned
+                />   
+            </div>
             
           </div>
         </div>
