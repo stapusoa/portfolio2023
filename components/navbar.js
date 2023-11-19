07d6fa256f49b4af9fa5f1763be135d83d6b3213
Sara Tapusoa <sara.tapusoa@gmail.com>
2023-11-18 20:18:08 -0700

    responsive menu

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,27 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
   const toggleMenu = () => {
-    const navbarRight = document.querySelector('.navbar-right');
-    navbarRight.classList.toggle('active');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('active');
   };
+
+  const handleClickOutside = (event) => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    if (mobileMenu && !mobileMenu.contains(event.target) && !event.target.matches('.menu-icon, .menu-icon *')) {
+      mobileMenu.classList.remove('active');
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav className="nav">
@@ -26,14 +41,23 @@ export default function Navbar() {
           <MenuIcon /> 
         </div>
         <div className="navbar-right">
-          <Link href="/">work
-          </Link>
-          <Link href="/about">about
-          </Link>
-          <Link href="/resume">resume
-          </Link>
+          <Link href="/">work</Link>
+          <Link href="/about">about</Link>
+          <Link href="/resume">resume</Link>
+        </div>
+        <div className="mobile-menu">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/services">Services</Link>
+          <Link href="/contact">Contact</Link>
         </div>
       </div>
     </nav>
   );
 }
+
+
+
+
+
+
